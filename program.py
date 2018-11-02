e591c589dd24d5e4e34b5a0162fdd709bffbbe13
dveleztx <dveleztx@gmail.com>
2018-11-02 12:28:31 -0500

    changed program

--- program.py
+++ program.py
@@ -41,36 +41,7 @@ def game_loop():
     # Play the Game
     while True:
 
-        # Random Encounter
-        active_creature = random.choice(creatures)
-        print("A {} of level {} has appeared from a dark and foggy forest..."
-              .format(active_creature.name, active_creature.level))
-        print()
 
-        # Input
-        cmd = input("Do you [a]ttack, [r]unaway, or [l]ook around? ").lower()
-        if cmd == "a":
-            if hero.attack(active_creature):
-                creatures.remove(active_creature)
-            else:
-                print("The wizard runs and hides taking time to recover...")
-                time.sleep(5)
-                print("The wizard returns revitalized!")
-        elif cmd == "r":
-            print("The wizard has become unsure of his power and flees!!!")
-        elif cmd == "l":
-            print("The wizard {} takes in the surroundings and sees: ".format(hero.name))
-            for c in creatures:
-                print(" * A {} of level {}".format(c.name, c.level))
-        elif cmd == "x":
-            print("Thanks for playing!")
-            break
-        else:
-            print("Incorrect entry!")
-
-        # If no creatures exist, end game
-        if not creatures:
-            print("You've defeated all the creatures, well done!")
 
         print()
 
