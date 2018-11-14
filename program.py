289ec8f2878178ab09f332bc3566d5b4ed799b81
dveleztx <dveleztx@gmail.com>
2018-11-13 23:28:59 -0600

    Added game logic back -DV

--- program.py
+++ program.py
@@ -36,12 +36,38 @@ def game_loop():
     ]
 
     # Create Hero Object
-    hero = Wizard("David", 10)
+    hero = Wizard("David", 1000)
 
     # Play the Game
     while True:
 
+        # Random Encounter
+        active_creature = random.choice(creatures)
+        print("A {} of level {} has appeared from a dark and foggy forest..."
+              .format(active_creature.name, active_creature.level))
+        print()
 
+        # User Input
+        cmd = input("Do you [a]ttack, [r]unaway, or [l]ook around? ").lower()
+
+        # Attack
+        if cmd == "a":
+            if hero.attack(active_creature):
+                creatures.remove(active_creature)
+            else:
+                print("The wizard runs and hides taking time to recover...")
+                time.sleep(5)
+                print("The wizard returns revitalized!!")
+        elif cmd == "r":
+            print("The wizard has become unsure of their power and flees!!!")
+        elif cmd == "l":
+            print("The wizard {} takes in the surroundings and sees: "
+                  .format(hero.name))
+            for c in creatures:
+                print(" * A {} of level {}".format(c.name, c.level))
+        else:
+            print("Thanks for playing!")
+            break
 
         print()
 
